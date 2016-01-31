c3e6bac8f77aec473475e63a188d97f92e32220b
Daneil Blyth <rdblyth@gmail.com>
2016-01-31 17:45:43 -0500

    Changed method name.

--- src/main/scala/Exercise2_1.scala
+++ src/main/scala/Exercise2_1.scala
@@ -1,16 +1,18 @@
+import scala.annotation.tailrec
+
 object Exercise2_1 {
 
   def fib(n: Int) : BigInt = {
 
-    @annotation.tailrec
-    def fib(n: Int, prev: BigInt = 0, next: BigInt = 1): BigInt = {
+    @tailrec
+    def fibTailRec(n: Int, prev: BigInt = 0, next: BigInt = 1): BigInt = {
       n match {
         case 0 => prev
         case 1 => next
-        case _ => fib((n - 1), next, prev + next)
+        case _ => fibTailRec((n - 1), next, prev + next)
       }
     }
 
-    fib(n)
+    fibTailRec(n)
   }
 }
