1408d5ebffb9695930e493bb80fb957f3b959c9f
Daneil Blyth <rdblyth@gmail.com>
2016-01-31 17:36:33 -0500

    Initial commit.

---
+++ src/test/scala/Exercise2_1Test.scala
@@ -0,0 +1,26 @@
+import org.scalatest.FunSuite
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+import org.scalatest.prop.Checkers
+
+@RunWith(classOf[JUnitRunner])
+class Exercise2_1Test extends FunSuite with Checkers {
+
+  test("Fibonacci Number for 0") {
+    assert(Exercise2_1.fibonacci(0) == 0)
+  }
+
+  test("Fibonacci Number for 1") {
+    assert(Exercise2_1.fibonacci(1) == 1)
+  }
+
+  test("Fibonacci Number for 2") {
+    assert(Exercise2_1.fibonacci(2) == 1)
+  }
+
+  test("Fibonacci Number for 9") {
+    assert(Exercise2_1.fibonacci(9) == 34)
+  }
+}
