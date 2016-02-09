0f288473129fe59318c01d522811060b6255f163
Daneil Blyth <rdblyth@gmail.com>
2016-02-08 21:57:23 -0500

    Cleaned up.

--- src/test/scala/chapter3/ListTest.scala
+++ src/test/scala/chapter3/ListTest.scala
@@ -1,6 +1,6 @@
 package chapter3
 
-
+import chapter3.List._
 import org.junit.runner.RunWith
 import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
@@ -10,60 +10,64 @@ import org.scalatest.prop.Checkers
 class ListTest extends FunSuite with Checkers {
 
   test("tail removes the first element in a List") {
-    assert(List.tail(List(1,2,3)) == List(2,3))
+    assert(tail(List(1,2,3)) == List(2,3))
   }
 
   test("tail on an empty List throws an Exception") {
     intercept[RuntimeException]{
-      List.tail(List())
+      tail(List())
     }
   }
 
   test("setHead replaces the first element of a List") {
-    assert(List.setHead(List(4,2,3), 1) == List(1,2,3))
+    assert(setHead(List(4,2,3), 1) == List(1,2,3))
   }
 
   test("setHead on an empty List throws an Exception") {
     intercept[RuntimeException]{
-      List.setHead(List(), 1)
+      setHead(List(), 1)
     }
   }
 
   test("drop with a value of 0 returns the passed in List") {
-    assert(List.drop(List(1,2,3), 0) == List(1,2,3))
+    assert(drop(List(1,2,3), 0) == List(1,2,3))
   }
 
   test("drop with a negative value returns the passed in List") {
-    assert(List.drop(List(1,2,3), -1) == List(1,2,3))
+    assert(drop(List(1,2,3), -1) == List(1,2,3))
   }
 
   test("drop removes first n elements from a List") {
-    assert(List.drop(List(1,2,3), 2) == List(3))
+    assert(drop(List(1,2,3), 2) == List(3))
   }
 
   test("dropWhile removes elements from a List prefix as long as they match a predicate") {
-    assert(List.dropWhile(List(1,2,3), (x: Int) => x < 3) == List(3))
+    assert(dropWhile(List(1,2,3), (x: Int) => x < 3) == List(3))
   }
 
   test("dropWhile on List where no elements match predicate returns the full List") {
-    assert(List.dropWhile(List(1,2,3), (x: Int) => x > 3) == List(1,2,3))
+    assert(dropWhile(List(1,2,3), (x: Int) => x > 3) == List(1,2,3))
   }
 
   test("dropWhile on List where all elements match predicate returns an empty List") {
-    assert(List.dropWhile(List(1,2,3), (x: Int) => x < 4) == List())
+    assert(dropWhile(List(1,2,3), (x: Int) => x < 4) == List())
   }
 
   test("init removes the last element from a List") {
-    assert(List.init(List(1,2,3)) == List(1,2))
+    assert(init(List(1,2,3)) == List(1,2))
   }
 
   test("init on a List with one element returns an empty List") {
-    assert(List.init(List(1)) == List())
+    assert(init(List(1)) == List())
   }
 
   test("init on an empty List throws an Exception") {
     intercept[RuntimeException]{
-      List.init(List())
+      init(List())
     }
+  }
+
+  test("length retruns the number of elements in a List") {
+    assert(length(List(1,2,3)) == 3)
   }
 }
